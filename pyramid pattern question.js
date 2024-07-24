df1bd576a115c432cb5346db6868f618c36e95d1
faizannasim <faizannasim59@gmail.com>
2024-07-24 22:44:09 +0530

    Update pyramid pattern question.js

--- pyramid pattern question.js
+++ pyramid pattern question.js
@@ -1,10 +1,10 @@
-let i,j,k;
-for(let i=1;i<=3;i++)
-    for(let k=1;k<=3-i;k++){
-        process.stdout.write(" ")
-{
-    for(let j=1;j<=i;j++){
-        process.stdout.write("*  ")
+let i, j, k;
+for (let i = 1; i <= 5; i++) {
+    for (let k = 1; k <= 5 - i; k++) {
+        process.stdout.write(" ");
     }
-    process.stdout.write("\n ")
+    for (let j = 1; j <= i; j++) {
+        process.stdout.write("* ")
+    }
+    process.stdout.write("\n")
 }
